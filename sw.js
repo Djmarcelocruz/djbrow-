8f6b3cfb8d8881a2a8675f71802e0275fb12891a
Djmarcelocruz <113671994+Djmarcelocruz@users.noreply.github.com>
2026-04-24 11:15:25 -0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -2,17 +2,38 @@ const CACHE_NAME = 'pacto-v1';
 const ASSETS = [
   './',
   './index.html',
-  './manifest.json'
+  './manifest.json',
+  './icon-512.png'
 ];
 
+// Instala o Service Worker e guarda os arquivos no cache
 self.addEventListener('install', (e) => {
   e.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
+    caches.open(CACHE_NAME).then((cache) => {
+      console.log('📦 Arquivos do Pacto salvos no cache!');
+      return cache.addAll(ASSETS);
+    })
   );
 });
 
+// Responde com o cache quando estiver offline
 self.addEventListener('fetch', (e) => {
   e.respondWith(
-    caches.match(e.request).then((res) => res || fetch(e.request))
+    caches.match(e.request).then((res) => {
+      return res || fetch(e.request);
+    })
+  );
+});
+
+// Limpa caches antigos se você atualizar a versão
+self.addEventListener('activate', (e) => {
+  e.waitUntil(
+    caches.keys().then((keyList) => {
+      return Promise.all(keyList.map((key) => {
+        if (key !== CACHE_NAME) {
+          return caches.delete(key);
+        }
+      }));
+    })
   );
 });
